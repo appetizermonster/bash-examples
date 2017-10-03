4015fb23b1838e6dd3fbe13f221c5af889a112d5
Heejin (macbook) <monster@teamappetizer.com>
2017-10-04 00:07:39 +0900

    Fix indentation at random-word.sh

--- random-word.sh
+++ random-word.sh
@@ -1,9 +1,9 @@
 #!/bin/bash 
 
 if [ -z $1 ]; then
-	echo "Please specify the number of random words"
-	echo "ex) $0 10"
-	exit 0
+  echo "Please specify the number of random words"
+  echo "ex) $0 10"
+  exit 0
 fi
 
 WORDS=/usr/share/dict/words
@@ -11,14 +11,14 @@ WORD_COUNT=$(wc -l $WORDS | awk '{print $1}')
 SCALE=$(( $WORD_COUNT/32767 ))
 
 pick_random_words() {
-	RANDOM_INDEX=$(( ($RANDOM*$SCALE)%$WORD_COUNT+1 )) 
-	RANDOM_WORD=$(sed -n $RANDOM_INDEX"p" < $WORDS)
-	echo $RANDOM_WORD
+  local RANDOM_INDEX=$(( ($RANDOM*$SCALE)%$WORD_COUNT+1 )) 
+  local RANDOM_WORD=$(sed -n $RANDOM_INDEX"p" < $WORDS)
+  echo $RANDOM_WORD
 }
 
 COUNT=$1
 for (( v=1; v<=$COUNT; v++ )); do
-	WORD=$(pick_random_words)
-	echo "$v. $WORD"
+  WORD=$(pick_random_words)
+  echo "$v. $WORD"
 done
 
